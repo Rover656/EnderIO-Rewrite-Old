bf25009e83437f454d61c91f3a1a8c7fe07ccb77
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2021-10-22 00:58:01 +0200

    Material Recipes
    Added all the material item recipes (that I could find).
    Didn't do any recipe with a block as result.
    Some items are still missing (like tools) and will need a recipe add a later date.

--- enderio-base/src/main/java/com/enderio/base/common/tag/EIOTags.java
+++ enderio-base/src/main/java/com/enderio/base/common/tag/EIOTags.java
@@ -1,3 +1,33 @@
 package com.enderio.base.common.tag;
 
-public class EIOTags {}
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.Tags.IOptionalNamedTag;
+
+public class EIOTags {
+    
+    public static void init(){
+        Items.init();
+        Blocks.init();
+    }
+    
+    public static class Items{
+        
+        private static void init(){}
+        
+        public static final IOptionalNamedTag<Item> DUSTS_LAPIS = ItemTags.createOptional(new ResourceLocation("forge", "dusts/lapis"));
+        public static final IOptionalNamedTag<Item> DUSTS_COAL = ItemTags.createOptional(new ResourceLocation("forge", "dusts/coal"));
+        public static final IOptionalNamedTag<Item> DUSTS_QUARTZ = ItemTags.createOptional(new ResourceLocation("forge", "dusts/quartz"));
+        public static final IOptionalNamedTag<Item> SILICON = ItemTags.createOptional(new ResourceLocation("forge", "silicon"));
+        public static final IOptionalNamedTag<Item> FUSED_QUARTZ = ItemTags.createOptional(new ResourceLocation("forge", "fused_quartz"));
+        public static final IOptionalNamedTag<Item> CLEAR_GLASS = ItemTags.createOptional(new ResourceLocation("forge", "clear_glass"));
+    }
+    
+    public static class Blocks{
+        
+        private static void init(){}
+        
+        //public static final IOptionalNamedTag<Block> FUSED_QUARTZ = BlockTags.createOptional(new ResourceLocation("forge", "fused_quartz"));
+    }
+}
