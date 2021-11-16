5e3f8a3c2de272031b41cc96c6dab201117718ea
Crazypants <crazypants.mc@gmail.com>
2021-11-16 17:18:05 +0800

    rebase

--- enderio-base/src/main/java/com/enderio/base/common/item/EIOItems.java
+++ enderio-base/src/main/java/com/enderio/base/common/item/EIOItems.java
@@ -5,6 +5,7 @@ import com.enderio.base.common.capability.capacitors.ICapacitorData;
 import com.enderio.base.common.item.capacitors.LootCapacitorItem;
 import com.enderio.base.common.item.darksteel.DarkSteelAxeItem;
 import com.enderio.base.common.item.darksteel.DarkSteelPickaxeItem;
+import com.enderio.base.common.item.darksteel.upgrades.DarkSteelUpgradeRegistry;
 import com.enderio.base.common.item.darksteel.upgrades.EmpoweredUpgrade;
 import com.enderio.base.common.item.darksteel.upgrades.ForkUpgrade;
 import com.enderio.base.common.item.darksteel.upgrades.SpoonUpgrade;
@@ -14,12 +15,7 @@ import com.enderio.base.common.item.misc.EnderfaceItem;
 import com.enderio.base.common.item.misc.GearItem;
 import com.enderio.base.common.item.misc.MaterialItem;
 import com.enderio.base.common.item.spawner.BrokenSpawnerItem;
-import com.enderio.base.common.item.tool.CoordinateSelectorItem;
-import com.enderio.base.common.item.tool.YetaWrenchItem;
-import com.enderio.base.common.tag.EIOTags;
-import com.enderio.base.common.item.tool.LevitationStaffItem;
-import com.enderio.base.common.item.tool.SoulVialItem;
-import com.enderio.base.common.item.tool.ElectromagnetItem;
+import com.enderio.base.common.item.tool.*;
 import com.enderio.base.common.tag.EIOTags;
 import com.enderio.base.data.model.item.ItemModelUtils;
 import com.tterrag.registrate.Registrate;
@@ -28,12 +24,14 @@ import com.tterrag.registrate.util.NonNullLazyValue;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import com.tterrag.registrate.util.nullness.NonNullFunction;
 import com.tterrag.registrate.util.nullness.NonNullSupplier;
-
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.Tag;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.ForgeTier;
@@ -41,6 +39,7 @@ import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.TierSortingRegistry;
 
 import java.util.List;
+import java.util.Objects;
 
 @SuppressWarnings("unused")
 public class EIOItems {
