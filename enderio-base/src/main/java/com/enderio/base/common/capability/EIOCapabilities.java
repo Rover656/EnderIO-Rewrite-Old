9d117950b54718e4d2f5e5f692d29ecfb23df8df
CrazyPants <crazypants.mc@gmail.com>
2021-11-16 19:43:50 +0800

    Update enderio-base/src/main/java/com/enderio/base/common/capability/EIOCapabilities.java
    Co-authored-by: agnor99 <36055315+agnor99@users.noreply.github.com>

--- enderio-base/src/main/java/com/enderio/base/common/capability/EIOCapabilities.java
+++ enderio-base/src/main/java/com/enderio/base/common/capability/EIOCapabilities.java
@@ -34,6 +34,5 @@ public class EIOCapabilities {
         event.register(ICapacitorData.class);
         event.register(IDarkSteelUpgradable.class);
         event.register(ICoordinateSelectionHolder.class);
-        event.register(IDarkSteelUpgradable.class);
     }
 }
